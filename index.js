e66c8d01474e73ff88727a2b46531467560056a2
alexmita04 <alexmita04@gmail.com>
2025-07-09 11:52:14 +0300

    add MongoDB connection setup

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  // await mongoose.connect(
+  //   `mongodb://${process.env.DB_USERNAME}:${process.envDB_PASSWORD}@127.0.0.1:27017/StudioScope`
+  // );
+
+  await mongoose.connect(
+    `mongodb+srv://alexmita04:${process.env.DB_PASSWORD}@cluster0.9mcrbtl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  );
+}
 
 const app = express();
 
