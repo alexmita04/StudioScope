367db4d49a337bf38e4c94f3483b091f0fd8f7ac
alexmita04 <alexmita04@gmail.com>
2025-07-09 11:40:49 +0300

    add and configure dotenv for environment variable management

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "./config.env" });
+  console.log(process.env.PORT);
+}
+
 const express = require("express");
 
 const app = express();
